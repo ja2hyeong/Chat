1fa60b3966e27b13b9474ae7c394886d45d3d191
User <jaeh@gmail.com>
2021-11-15 23:52:34 +0900

    chat

---
+++ app/src/main/java/com/example/chat/models/User.java
@@ -0,0 +1,8 @@
+package com.example.chat.models;
+
+import java.io.Serializable;
+import java.lang.String;
+
+public class User implements Serializable {
+    public String name, image, email, token, id;
+}
